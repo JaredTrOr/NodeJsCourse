76594005f29e6c2a018d0caf9bceddf2e828e1fd
JaredTrOr <trujillojaredalexander@gmail.com>
2022-12-15 03:02:00 -0600

    It was corrected the requesting problem

--- 8.-HttpModules.js
+++ 8.-HttpModules.js
@@ -8,11 +8,14 @@ const server = http.createServer((req,res) => {
     else if(req.url === '/about'){
         res.end('About page');
     }
-    res.end(`
+    else{
+        res.end(`
         <h1>Oops...</h1>
         <p>We can't seem to find the page you are looking for</p>
         <a href='/'>Home</a>
     `);
+    }
+    
 });
 
 server.listen(PORT);
